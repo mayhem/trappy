8d0143a2daeeccfa53b1b1ff8ed88761667ba131
Robert Kaye <rob@musicbrainz.org>
2024-05-09 11:53:53 +0200

    More gradients

--- rpi/gradient_scroller.py
+++ rpi/gradient_scroller.py
@@ -1,19 +1,35 @@
 from abc import abstractmethod
+from math import fmod
+from colorsys import hsv_to_rgb
+from time import sleep, monotonic
+
 from gradient import Gradient
 from random import random
-from time import sleep, monotonic
-from colorsys import hsv_to_rgb
 from effect import Effect
+
+
+def hue_to_rgb(hue):
+    r,g,b = hsv_to_rgb(fmod(hue, 1.0), 1.0, 1.0)
+    return (int(r * 255), int(g * 255), int(b * 255))
 
 
 class EffectGradientChase(Effect):
 
-    def point_generator(self, offset, row_index):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.hue = 0.0
+
+    def point_generator_stripes(self, offset, row_index):
+        self.hue += .01
         if row_index % 2 == 0:
-            color = (255, 60, 0)
+            color = hue_to_rgb(self.hue)
         else:
-            color = (255, 0, 255)
+            color = hue_to_rgb(self.hue + .5)
         return (offset, color)
+
+    def point_generator_rainbow(self, offset, row_index):
+        self.hue += .2
+        return (offset, hue_to_rgb(self.hue))
 
     def shift(self, palette, dist):
         shifted = []
@@ -34,16 +50,16 @@ class EffectGradientChase(Effect):
         shift_dist = .02
         palette = []
         while True:
-            palette.append(self.point_generator(offset, row_index))
+            palette.append(self.point_generator_stripes(offset, row_index))
             row_index += 1
             offset += spacing
             if palette[-1][0] > 1.5:
                 break
 
         g = Gradient(palette)
-        for i in range(1000): 
-            if monotonic() > timeout:
-                return
+        while True:
+#            if monotonic() > timeout:
+#                return
 
             buf = []
             for k in range(self.driver.strips):
