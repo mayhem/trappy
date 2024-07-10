b63f1caf517788af00c748b795e72188a20d55c4
Robert Kaye <rob@musicbrainz.org>
2024-07-10 20:59:43 +0200

    Now for a workable baseline

--- rpi/apc_mini_mapping.py
+++ rpi/apc_mini_mapping.py
@@ -4,31 +4,57 @@ from colorsys import hsv_to_rgb
 from time import sleep
 import json
 
-from colors import sort_colors, simple_color_gradient
 import rtmidi
 
-def set_color_grid(m_out, page):
-    msg = [0xF0, 0x47, 0x7F, 0x4F, 0x24]
+def init_color_grid(m_out):
     pad_msg = []
-    if page == 0:
-        r = (0, 32)
-    else:
-        r = (32, 64)
-    for pad in range(r[0], r[1]):
-        hue = (pad + 1) / 64
-        r,g,b = hsv_to_rgb(hue, 1.0, 1.0)
-        r = int(r * 255)
-        g = int(g * 255)
-        b = int(b * 255)
+    for pad in range(0, 64):
+        hue = 0
+        if pad < 8:
+            r,g,b = (0,0,0)
+        else:
+            hue = (pad - 7) / 56
+            r,g,b = hsv_to_rgb(hue, 1.0, 1.0)
+            r = int(r * 255)
+            g = int(g * 255)
+            b = int(b * 255)
         pad_msg.extend([pad, pad, r >> 7, r & 0x7F, g >> 7, g & 0x7F, b >> 7, b & 0x7F])
 
-    num_bytes = len(pad_msg)
+    num_bytes = len(pad_msg) // 2
+
+    msg = [0xF0, 0x47, 0x7F, 0x4F, 0x24]
     msg.extend((num_bytes >> 7, num_bytes & 0x7F))
-    msg.extend(pad_msg)
+    msg.extend(pad_msg[0:256])
+    msg.append(0xF7)
+    m_out.send_message(msg)
+    sleep(.005)
+
+    msg = [0xF0, 0x47, 0x7F, 0x4F, 0x24]
+    msg.extend((num_bytes >> 7, num_bytes & 0x7F))
+    msg.extend(pad_msg[256:512])
+    msg.append(0xF7)
+    m_out.send_message(msg)
+    sleep(.005)
+
+def set_pad_color(m_out, pad, color):
+    num_bytes = 8
+    msg = [0xF0, 0x47, 0x7F, 0x4F, 0x24]
+    msg.extend((num_bytes >> 7, num_bytes & 0x7F))
+    msg.extend([pad, pad, color[0] >> 7, color[0] & 0x7F, color[1] >> 7, color[1] & 0x7F, color[2] >> 7, color[2] & 0x7F])
     msg.append(0xF7)
 
     m_out.send_message(msg)
+    sleep(.005)
 
+def clear_pads(m_out):
+    num_bytes = 8
+    msg = [0xF0, 0x47, 0x7F, 0x4F, 0x24]
+    msg.extend((num_bytes >> 7, num_bytes & 0x7F))
+    msg.extend([0, 63, 0, 0, 0, 0, 0, 0])
+    msg.append(0xF7)
+
+    m_out.send_message(msg)
+    sleep(.005)
 
 m_out = rtmidi.MidiOut()
 m_in = rtmidi.MidiIn()
@@ -38,7 +64,7 @@ if available_ports:
     m_in.open_port(1)
 
 with m_out:
-    set_color_grid(m_out, 0)
-    set_color_grid(m_out, 1)
+    clear_pads(m_out)
+    init_color_grid(m_out)
     
 del m_out
