b69c972d57d8d9be39ce02cdcb0108b8e8b30a6c
Robert Kaye <rob@musicbrainz.org>
2024-09-08 23:57:46 +0200

    Sparkles now can set the fade constant

--- rpi/sparkles.py
+++ rpi/sparkles.py
@@ -12,6 +12,7 @@ from color import hue_to_rgb, random_color
 class EffectSparkles(Effect):
 
     FADER_NUM_DOTS = 3
+    FADER_FADE = 4
     FADE_CONSTANT = .85
 
     def __init__(self, driver, event, apc = None, timeout=None):
@@ -23,6 +24,10 @@ class EffectSparkles(Effect):
     def map_fader_value(self, fader, value):
         if fader == self.FADER_NUM_DOTS:
             return int(value * ((self.driver.strips * 2) - 1) + 1)
+
+        # unsure what is needed here
+        if fader == self.FADER_FADE:
+            return value
 
         return None
 
@@ -38,14 +43,16 @@ class EffectSparkles(Effect):
                 sleep(.01)
                 continue
 
+            num_dots = int(self.fader_value(self.FADER_NUM_DOTS))
+            fade_constant = self.fader_value(self.FADER_FADE)
+
             # Fade out existing data
             for i in range(self.driver.strips * self.driver.leds):
                 color = led_data[i]
                 for j in range(3):
-                    color[j] = int(float(color[j]) * self.FADE_CONSTANT)
+                    color[j] = int(float(color[j]) * fade_constant)
                 led_data[j] = color
 
-            num_dots = int(self.fader_value(self.FADER_NUM_DOTS))
 
             # Add more sparkles
             strips = [ x for x in range(self.driver.strips)]
