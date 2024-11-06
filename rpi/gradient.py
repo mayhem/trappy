53a26cdcadc5bfdba0f35e1a8ed20d41660e5d00
Robert Kaye <rob@musicbrainz.org>
2024-11-07 00:40:04 +0100

    Try a new easier to use gradient

--- rpi/gradient.py
+++ rpi/gradient.py
@@ -41,12 +41,7 @@ class Gradient(object):
         if offset < 0.0 or offset > 1.0:
             raise IndexError("Invalid offset %f" % offset)
 
-        for index in range(len(self.palette)):
-
-            # skip the first item
-            if index == 0:
-                continue
-
+        for index in range(1, len(self.palette)):
             if self.palette[index][0] >= offset:
                 section_begin_offset = self.palette[index - 1][0]
                 section_end_offset = self.palette[index][0]
@@ -61,4 +56,42 @@ class Gradient(object):
                 return (max(min(new_color[0], 255), 0), max(min(new_color[1], 255), 0), max(min(new_color[2], 255), 0))
 
         self.print_palette()
+        assert False
+
+class WeightedGradient(object):
+
+    def __init__(self, palette):
+        self.palette = palette
+        total_sum = sum([ w for w, col in palette])
+        self.points = [ 0.0 ] 
+        total = 0
+        for w, col in palette:
+            width = w / total_sum
+            self.points.append(total + width)
+            total += width
+
+    def print_palette(self, palette=None):
+        print("Gradient palette:")
+        for pal in palette or self.palette:
+            print("%.3f: %d, %d, %d" % (self.points, pal[1][0], pal[1][1], pal[1][2]))
+        print()
+
+    def get_color(self, offset):
+        """ Palette format is: (weight, color) -> (4, (255, 0, 28)) """
+        assert offset >= 0.0 and offset <= 1.0
+
+        for index in range(1, len(self.palette)):
+            if self.points[index] >= offset:
+                section_begin_offset = self.points[index - 1]
+                section_end_offset = self.points[index]
+
+                percent = (offset - section_begin_offset) / (section_end_offset - section_begin_offset)
+                new_color = [ 
+                    int(self.palette[index - 1][1][0] + ((self.palette[index][1][0] - self.palette[index - 1][1][0]) * percent)),
+                    int(self.palette[index - 1][1][1] + ((self.palette[index][1][1] - self.palette[index - 1][1][1]) * percent)),
+                    int(self.palette[index - 1][1][2] + ((self.palette[index][1][2] - self.palette[index - 1][1][2]) * percent))
+                ]
+                return (max(min(new_color[0], 255), 0), max(min(new_color[1], 255), 0), max(min(new_color[2], 255), 0))
+
+        print_palette()
         assert False
