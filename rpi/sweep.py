57406699b85c1bb4a6926075d265c1b92f03c47f
Robert Kaye <rob@musicbrainz.org>
2024-11-12 23:45:30 +0100

    Sweep no longer sucks!

--- rpi/sweep.py
+++ rpi/sweep.py
@@ -1,5 +1,5 @@
 from abc import abstractmethod
-from colorsys import hsv_to_rgb
+from colorsys import hsv_to_rgb, rgb_to_hsv
 from math import fmod
 from random import random, randint, shuffle
 from time import sleep, monotonic
@@ -8,6 +8,17 @@ from gradient import Gradient
 from effect import Effect, SpeedEvent, FaderEvent, DirectionEvent
 from color import hue_to_rgb, random_color
 
+def shift_color(color, shift):
+    h, s, v = rgb_to_hsv(color[0] / 255, color[1] / 255, color[2] / 255)
+    h = fmod(h + shift, 1.0)
+    r, g, b = hsv_to_rgb(h, s, v)
+    return (int(255 * r), int(255 * g), int(255 * b))
+
+def opposite_color(color):
+    return shift_color(color, .5)
+
+def tri_color(color):
+    return shift_color(color, .33333), shift_color(color, .66666)
 
 class EffectSweep(Effect):
 
@@ -38,6 +49,7 @@ class EffectSweep(Effect):
         strip = 0
         step = 1
         color = self.get_next_color()
+        color_1, color_2 = tri_color(color)
         while not self.stop:
             if self.timeout is not None and monotonic() > self.timeout:
                 return
@@ -51,6 +63,7 @@ class EffectSweep(Effect):
             if strip == 0 and step < 0:
                 step = 1
                 color = self.get_next_color()
+                color_1, color_2 = tri_color(color)
 
             for l in range(self.driver.leds):
                 led_data[(strip * self.driver.leds) + l] = color
@@ -58,10 +71,13 @@ class EffectSweep(Effect):
 
             for l in range(self.driver.leds):
                 r = randint(0, 25)
-                if r >= 3:
+                if step == 1 or r >= 3:
                     led_data[(strip * self.driver.leds) + l] = (0,0,0)
                 else:
-                    led_data[(strip * self.driver.leds) + l] = (255,255,255)
+                    if r % 2 == 0:
+                        led_data[(strip * self.driver.leds) + l] = color_1
+                    else:
+                        led_data[(strip * self.driver.leds) + l] = color_2
 
             strip = (strip + step) % self.driver.strips
             self.sleep()
