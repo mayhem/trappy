243c3cea9ed96fab9fe1760f764a639f761b5197
Robert Kaye <rob@musicbrainz.org>
2025-06-11 20:38:24 +0200

    Cleanup

--- scorpio/checkerboard/color.h
+++
@@ -1,8 +0,0 @@
-#ifndef __COLOR_H__
-#define __COLOR_H__
-
-#include "defs.h"
-
-void random_color(color_t *col);
-
-#endif
--- scorpio/checkerboard/gradient.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GRADIENT_H__
-#define __GRADIENT_H__
-
-#include "defs.h"
-
-void gradient_color(gradient_t *grad, float offset, color_t *dest);
-
-#endif
